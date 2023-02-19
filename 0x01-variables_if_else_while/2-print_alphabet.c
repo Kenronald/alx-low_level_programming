0d09b2847db889a2304cf7391fe1662360221fbe
Kenronald <akporonald@gmail.com>
2023-02-18 21:55:56 -0800

    2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<time.h>
+#include<stdlib.h>
+/**
+*main - A program that prints last digits
+*Return: 0 (Exit_SUCCESS)
+*/
+int main(void)
+{
+c = 'a';
+aa = '\n';
+while (c <= 'z')
+{
+putchar(c);
+putchar(aa);
+c = c + 1;
+}
