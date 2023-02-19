36f5d7ed161c11d716c0c4ab62d917ad4e4c3473
Kenronald <akporonald@gmail.com>
2023-02-19 00:30:02 -0800

    8-print_base16.c

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include<time.h>
+#include<stdlib.h>
+/**
+*main - A program that prints lower letters
+*Return: 0 (Exit_SUCCESS)
+*/
+int main(void)
+{
+int i, c;
+for (i = 0; i < 10; i++)
+{
+putchar(48 + i);
+}
+for (c = 'a'; c <= 'f'; c++)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
+}
